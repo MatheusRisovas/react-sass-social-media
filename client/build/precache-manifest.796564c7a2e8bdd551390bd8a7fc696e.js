da1a417eadded604be6145cae43835f04c95efdf
MatheusRisovas <huskysincero@gmail.com>
2019-06-11 21:23:19 -0300

    .

---
+++ client/build/precache-manifest.796564c7a2e8bdd551390bd8a7fc696e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ecb3670241caab957c6c398db3fc146",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f125a9339f7597718190",
+    "url": "/static/css/main.9482a9ab.chunk.css"
+  },
+  {
+    "revision": "b91cb499b772c360da1d",
+    "url": "/static/js/2.ae6338d2.chunk.js"
+  },
+  {
+    "revision": "f125a9339f7597718190",
+    "url": "/static/js/main.0d496af9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
